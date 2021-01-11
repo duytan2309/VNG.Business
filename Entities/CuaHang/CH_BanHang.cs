78f19fda757a91cddc9c6bcae1ba7853a3c5103d
Duy Tan <nhjdaubo@outlook.com>
2021-01-11 08:50:31 +0700

    up

--- Entities/CuaHang/CH_BanHang.cs
+++ Entities/CuaHang/CH_BanHang.cs
@@ -4,43 +4,44 @@ namespace VNG.Business.Entities.CuaHang
 {
     public class CH_BanHang
     {
-        public string status { get; set; }
-        public string TinhTrang { get; set; }
+
         public int Id { get; set; }
         public string MaCuaHang { get; set; }
         public int Ca { get; set; }
-        public string DiaChi { get; set; }
         public string SoHoaDonBan { get; set; }
         public string MaKhachHang { get; set; }
-        public string HoTen { get; set; }
-        public string SoDienThoai { get; set; }
         public string MaNhanVien { get; set; }
-        public string HoTenNV { get; set; }
         public DateTime? NgayBan { get; set; }
         public string Kieu { get; set; }
         public string Size { get; set; }
-        public string Label_KieuSize { get; set; }
         public int? SoLuong { get; set; }
         public decimal? Gia { get; set; }
-        public decimal? Gia2 { get; set; }
-        public decimal? ThanhTien { get; set; }
-        public decimal? ThanhTien2 { get; set; }
         public decimal? ThanhToan { get; set; }
         public decimal? TongThanhTien { get; set; }
         public decimal? TienKhachTra { get; set; }
         public decimal? TienGiam { get; set; }
-        public decimal? SoTienThua { get; set; }
         public decimal? ThucNhan { get; set; }
-        public int? ChiecKhau { get; set; }
         public string MaPhieuTang { get; set; }
-        public decimal? TienPhieuTang { get; set; }
         public int? TGBH { get; set; }
         public string CreateBy { get; set; }
         public DateTime? CreateDate { get; set; }
         public string ModifiedBy { get; set; }
         public DateTime? ModifiedDate { get; set; }
         ////////////////////
+        public string status { get; set; }
+        public string TinhTrang { get; set; }
+        public string DiaChi { get; set; }
         public decimal? TongTien { get; set; }
         public string StrTongTien { get; set; }
+        public decimal? Gia2 { get; set; }
+        public decimal? ThanhTien { get; set; }
+        public decimal? ThanhTien2 { get; set; }
+        public string Label_KieuSize { get; set; }
+        public string HoTen { get; set; }
+        public string SoDienThoai { get; set; }
+        public string HoTenNV { get; set; }
+        public decimal? TienPhieuTang { get; set; }
+        public int? ChiecKhau { get; set; }
+        public decimal? SoTienThua { get; set; }
     }
 }
--- Entities/KhoTong/KT_TonKho.cs
+++ Entities/KhoTong/KT_TonKho.cs
@@ -2,15 +2,12 @@
 {
     public class KT_TonKho
     {
-        public string status { get; set; }
         public int Id { get; set; }
         public string NoiTon { get; set; }
-        public string TenChungLoai { get; set; }
         public string Kieu { get; set; }
         public string Size { get; set; }
-        public decimal? GiaNhap { get; set; }
-        public decimal? ThanhTien { get; set; }
         public int? PhanThung { get; set; }
+        /// ////////////////////////////////////////////////////      
         public int? SLTon { get; set; }
         public int? TC { get; set; }
         public int Size00 { get; set; }
@@ -32,11 +29,9 @@
         public int Size44 { get; set; }
         public int Size45 { get; set; }
         public int Size46 { get; set; }
-
-        // Kiểm tra số lượng tồn với số lượng truyền vào 
-        /// <summary>
-        /// >= 0 : SoLuong > SLTon , < 0: SoLuong> SLTon
-        /// </summary>
-        public int? EqualSoLuong_SLTon { get; set; }
+        public string TenChungLoai { get; set; }
+        public decimal? ThanhTien { get; set; }
+        public decimal? GiaNhap { get; set; }
+        public string status { get; set; }
     }
 }
